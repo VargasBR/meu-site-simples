f5504ac5b591e49457c411f56808eecc4c1812e9
VargasBR <alvaro_inacio@yahoo.com.br>
2026-04-28 23:05:59 -0300

    Meu primeiro commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Meu Primeiro Site PaaS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Olá! Este é meu site hospedado em um PaaS.</h1>
+    <p>Data e hora atual: <?php echo date('d/m/Y H:i:s'); ?></p>
+    <script src="script.js"></script>
+</body>
+</html>
